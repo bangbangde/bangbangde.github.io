a0cfe4c69c040974347cdb6146610a172c39c73c
Frank <chengqifw@gmail.com>
2023-03-06 16:58:31 +0800

    reset by vitepress

--- playground/CRLF/main.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const { resolve } = require('path');
-
-const strLF = fs.readFileSync(resolve(__dirname, 'LF'), 'utf-8');
-const strCRLF = fs.readFileSync(resolve(__dirname, 'CRLF'), 'utf-8');
-console.log(encodeURI(strLF), strLF.length);
-console.log(encodeURI(strCRLF), strCRLF.length);
