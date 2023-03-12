6250b8c6120472e328b80edb028c7f6ef96faf63
Frank <chengqifw@gmail.com>
2023-03-13 01:21:08 +0800

    update

--- docs/yuque/[pkg].paths.js
+++ docs/yuque/[pkg].paths.js
@@ -3,14 +3,23 @@ import { loadEnv } from 'vitepress';
 const env = loadEnv('', process.cwd(), "");
 
 const fetchData = async () => {
-  const res = await (await fetch('https://www.yuque.com/api/v2/repos/zhaochengqi/puhf6g/toc', {
+  return fetch('https://www.yuque.com/api/v2/repos/zhaochengqi/puhf6g/toc', {
     headers: {
       'X-Auth-Token': env['X-Auth-Token'],
       'User-Agent': 'vitepress',
       'Content-Type': 'application/json'
     }
-  })).json();
-  return res;
+  }).then(res => res.json())
+    .then(data => {
+      if (data.status === 200) return data;
+      console.log(data);
+      return { data: [] };
+    }).catch(err => {
+      console.error(err);
+      return {
+        data: []
+      }
+    })
 }
 
 export default {
