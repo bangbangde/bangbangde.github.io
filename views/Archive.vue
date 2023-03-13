a3169764229859c29b7cb89ff0b5f4184826dee4
Frank <chengqifw@gmail.com>
2023-03-13 14:44:11 +0800

    update

--- views/Archive.vue
+++ views/Archive.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="notes-home">
-    <ul class="archive-ul">
-      <li class="archive-li" v-for="(item, index) in data" :key="index">
-        <a class="archive-doc" :href="item.href">
-          <div class="doc-info">
-            <span class="doc-title">{{ item.title }}</span>
-            <span class="dashed-line"></span>
-            <span class="doc-date">{{ item.updatedByGit }}</span>
-          </div>
-        </a>
-        <div class="doc-tags">
-          <span class="doc-tag" v-for="tag in item.tags" :key="tag">{{ tag }}</span>
+    <h2 class="title">笔记·最近编辑</h2>
+    <div class="layout-flex">
+      <div class="content">
+        <ul class="archive-ul">
+          <li class="archive-li" v-for="(item, index) in list" :key="index">
+            <a class="archive-doc" :href="item.href">
+              <div class="doc-info">
+                <span class="doc-title">{{ item.title }}</span>
+                <span class="dashed-line"></span>
+                <span class="doc-date">{{ item.updatedByGit }}</span>
+              </div>
+            </a>
+            <div class="doc-tags">
+              <span class="doc-tag" v-for="tag in item.tags" :key="tag">{{ tag }}</span>
+            </div>
+            <div class="doc-desc">{{ item.description }}</div>
+          </li>
+        </ul>
+      </div>
+      <div class="aside">
+        <div class="aside-title">tags</div>
+        <div class="tags">
+          <span
+            class="tag"
+            :class="{active: selectedTags.includes(k)}" v-for="(v, k) in tagsAndCategories.tags"
+            :key="k"
+            @click="() => handleTagOrCategoryClick('tag', k)"
+          >{{ k }}({{ v }})</span>
         </div>
-        <div class="doc-desc">{{ item.description }}</div>
-      </li>
-    </ul>
-
-    <div class="aside">
-      <div>
-        <span v-for="(v, k) in tagsAndCategories.tags" :key="tags">{{ k }}({{ v }})</span>
+        <div class="aside-title">categories</div>
+        <div class="categories">
+          <span
+            class="category"
+            :class="{active: selectedCategories.includes(k)}" v-for="(v, k) in tagsAndCategories.categories"
+            :key="k"
+            @click="() => handleTagOrCategoryClick('category', k)"
+          >{{ k }}({{ v }})</span>
+        </div>
       </div>
     </div>
   </div>
@@ -39,7 +58,9 @@ export default {
     return {}
   },
   data: () => ({
-    sortBy: SORT.LAST_UPDATED
+    sortBy: SORT.LAST_UPDATED,
+    selectedTags: [],
+    selectedCategories: []
   }),
   props: {
     source: Array
@@ -51,7 +72,7 @@ export default {
         const { title, tags, categories, description, created, updated } = data;
         return {
           file,
-          title: title || match[2] || match[3],
+          title: title || (match[2] && match[2] + '/index') || match[3],
           tags,
           categories,
           description,
@@ -61,6 +82,23 @@ export default {
           lastUpdated,
           href: match[1] + '.html'
         }
+      })
+    },
+    list() {
+      return this.data.filter(item => {
+        const filter = []
+        if (this.selectedTags.length) {
+          filter[0] = item.tags.some(v => this.selectedTags.includes(v));
+        } else {
+          filter[0] = true;
+        }
+        if (this.selectedCategories.length) {
+          filter[1] = item.categories.some(v => this.selectedCategories.includes(v));
+        } else {
+          filter[1] = true;
+        }
+        console.log(item, filter);
+        return filter[0] === true && filter[1] === true;
       }).sort((a, b) => {
         switch (this.sortBy) {
           case SORT.TITLE: break;
@@ -88,6 +126,17 @@ export default {
       });
       return data;
     }
+  },
+  methods: {
+    handleTagOrCategoryClick(type, value) {
+      const data = type === 'tag' ? this.selectedTags : this.selectedCategories;
+      const i = data.indexOf(value);
+      if (i >= 0) {
+        data.splice(i, 1)
+      } else {
+        data.push(value)
+      }
+    }
   }
 }
 </script>
@@ -96,16 +145,21 @@ export default {
 .notes-home {
   max-width: calc(var(--vp-layout-max-width) - 64px);
   margin: 0 auto;
+  padding: 0 24px;
+  @media (min-width: 768px) {
+    padding: 0 32px;
+  }
+}
+.layout-flex {
   display: flex;
 }
-.archive-ul {
-  padding: 0 24px;
-  flex: 1 1 auto;
+.notes-home .title {
+  font-size: 18px;
+  font-weight: 700;
+  margin: 18px 0;
 }
-@media (min-width: 768px) {
-  .archive-ul {
-      padding: 0 32px;
-  }
+.content {
+  flex: 1 1 auto;
 }
 
 .archive-li {
@@ -140,5 +194,32 @@ export default {
 .doc-desc {
   color: #86909c;
   font-size: 13px;
+}
+.aside {
+  margin-left: 24px;
+  width: 360px;
+  flex: 0 0 auto;
+}
+
+.tags, .categories {
+  margin: 16px 0 32px;
+}
+
+.tag, .category {
+  display: inline-block;
+  max-width: 100px;
+  word-break: break-all;
+  background-color: white;
+  font-size: 14px;
+  border-radius: 11px;
+  padding: 4px 8px;
+  margin: 8px;
+  line-height: 18px;
+  color: #333333;
+  cursor: pointer;
+}
+.tag.active, .category.active {
+  background-color: coral;
+  color: white;
 }
 </style>
